62f5d0a2290a7261495ad03786fea894924263a1
戴运鹏 <>
2018-06-15 18:05:22 +0800

    类

--- hello.py
+++ hello.py
@@ -383,22 +383,56 @@ def f1(s):
                 break;
     return a
 
->>> def f2(x):
+def f2(x):
     s = str(x)
     if f1(s):
     return x
 SyntaxError: expected an indented block
->>> def f2(x):
+ def f2(x):
     s = str(x)
     if f1(s):
         return x
 
+#排序
+from operator import itemgetter
+L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+sorted(L,key=itemgetter(0))
+sorted(L,key=itemgetter(1))
 
+#函数作为参数返回  --->闭包
+def lazy_sum(*args):
+    def sum():
+        ax = 0
+        for n in args:
+            ax = ax + n
+        return ax
+    return sum
 
+f = lazy_sum(1,2,4,5)
 
+# ******返回闭包时牢记一点：返回函数不要引用任何循环变量，或者后续会发生变化的变量。
 
+#匿名函数
+list(map(lambda x: x * x, [1, 2, 3, 4, 5, 6, 7, 8, 9]))#关键字lambda表示匿名函数，冒号前面的x表示函数参数。 匿名函数有个限制，就是只能有一个表达式，不用写return，返回值就是该表达式的结果。用匿名函数有个好处，因为函数没有名字，不必担心函数名冲突。此外，匿名函数也是一个函数对象，也可以把匿名函数赋值给一个变量，再利用变量来调用该函数：
 
+#装饰器
+#函数对象有一个__name__属性，可以拿到函数的名字
+import functools
 
+def log(text):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            print('%s %s():' % (text, func.__name__))
+            return func(*args, **kw)
+        return wrapper
+    return decorator
+#偏函数
+import functools
+int2 = functools.partial(int, base=2)
+#返回的int2函数和 下边的函数效果一样
+def int2(x, base=2):
+    return int(x, base)
 
 
 
